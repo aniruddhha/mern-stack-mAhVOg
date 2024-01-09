3c1dba3214a26ccc3be7a05c84c163c4d35bd041
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-09 18:30:56 +0530

    javascript: variable and data types 02

---
+++ day-31/app-02.js
@@ -0,0 +1,22 @@
+console.log('Numeric Type')
+var num = 12 // numeric 
+console.log(num)
+
+console.log('Decimal Numeric Type')
+var sal = 23.45 // numeric
+console.log(sal)
+
+console.log('Boolean Type')
+var isOkay = true // boolean 
+console.log(isOkay)
+
+console.log('String Type')
+var nm = 'Atomic Habits' // string
+console.log(nm)
+
+console.log('String Type')
+var isbn = "h235899" // string
+console.log(isbn)
+
+
+
