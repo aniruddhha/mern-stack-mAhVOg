ebf9e16b93d5f84bb40f49e50adda2128f2f1540
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-09 18:20:55 +0530

    javascript: variables 01

---
+++ day-31/app-01.js
@@ -0,0 +1,13 @@
+var num1 = 10
+num1 = 20 // reassigned 
+console.log('Value of Num1: ')
+console.log(num1)
+
+var num2 = 20
+num2 = 30  // reassigned 
+console.log('Value of Num2: ')
+console.log(num2)
+
+var result = num1 + num2
+console.log('Addition is ')
+console.log(num1 + num2)
