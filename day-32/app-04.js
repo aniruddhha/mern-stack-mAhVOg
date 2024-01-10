04f02e45ef2b91591b271f8450d754dc0a92a9e1
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-10 18:44:24 +0530

    javascript: functions basics

---
+++ day-32/app-04.js
@@ -0,0 +1,16 @@
+// Calculate the percentage of given marks
+
+// var marks = 60
+// var toatlMarks = 100
+// var percentage = (marks/toatlMarks) * 100
+
+// console.log('You have got '+ percentage)
+
+
+function calcPerc(mrk, ttlMrk) {
+    var per = (mrk/ttlMrk) * 100;
+    return per
+}
+
+var res = calcPerc(43, 60)
+console.log('Your percentage is '+ res)
