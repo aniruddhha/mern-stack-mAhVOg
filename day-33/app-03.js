614b25a2bfa988a0deb6de5bff009ec531832791
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-11 18:57:00 +0530

    javascript: vars: naming conventions

---
+++ day-33/app-03.js
@@ -0,0 +1,18 @@
+// invalid
+//var 10abc = 11
+//var a-b-c = 30
+
+// valid
+var abc = 11
+var _abc = 20
+var ABC = 90
+var $abc = 90
+var a_b_c = 90
+var a_10_b_20_c_30 =90
+
+// standard
+var employeeName = 'abc' // camel casing
+var empnm = 'pqr' // 
+
+var salaryAccountDetails = 1234
+
