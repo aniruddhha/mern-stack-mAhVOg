616b66c50189667b2db16b2f3ffb2cd016bd3e08
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-16 18:48:25 +0530

    javascript: if-else ladder, nesting

---
+++ day-36/app-03.js
@@ -0,0 +1,9 @@
+const marks = 20
+
+if( marks > 40 ) {
+    // if condition is true, it will execute code in this brackets
+    console.log('Pass')
+} else {
+    // if condition is false, it will execute code in this brackets
+    console.log('Fail')
+}
