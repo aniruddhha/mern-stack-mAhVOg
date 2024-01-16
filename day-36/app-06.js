3995de28a18798f877da8269da086cd848d3f691
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-16 18:54:39 +0530

    javascript: switch case

---
+++ day-36/app-06.js
@@ -0,0 +1,36 @@
+const day = 100 
+
+switch(day) {
+
+    case 1 : {
+        console.log('MON')
+        break
+    }
+    case 2 : {
+        console.log('TUE')
+        break
+    }
+    case 3 : {
+        console.log('WED')
+        break
+    }
+    case 4 : {
+        console.log('THU')
+        break
+    }
+    case 5 : {
+        console.log('FRI')
+        break
+    }
+    case 6 : {
+        console.log('SAT')
+        break
+    }
+    case 7 : {
+        console.log('SUN')
+        break
+    }
+    default: {
+        console.log('Invalid Day')
+    }
+}
--- day-36/readme.md
+++ day-36/readme.md
@@ -2,3 +2,8 @@ We have covered following topics today
 
 - ES6: var, let and const
 - Conditional Statements
+    - if-else
+        - if-else ladder
+        - nested if else
+    - switch-case
+
