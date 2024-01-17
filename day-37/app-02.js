24af563e2af26c85eff1fff1fe323642cddbd7dc
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-17 18:35:52 +0530

    javascript: ternary operators

---
+++ day-37/app-02.js
@@ -0,0 +1,17 @@
+
+function checkResult1(marks) {
+    if(marks >= 40) {
+        return "pass"
+    } 
+    return "fail"
+}
+
+function checkResult2(marks) {
+    return (marks >= 40) ? "pass" : "fail"
+}
+
+const res = checkResult2(20)
+console.log("You are " + res)
+
+
+
