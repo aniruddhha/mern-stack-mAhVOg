944eca7a97dde0c377a249d1ba1e1b626d60e862
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-17 18:26:09 +0530

    javascript: truthy and falsy values

---
+++ day-37/app-01.js
@@ -0,0 +1,25 @@
+// about strings
+// const something = '123' truthy value
+// const something = 'abc' truthy value
+// const something = '' falsy value
+
+// about numbers
+// const something = 12 truthy value
+// const something = -12 truthy value
+// const something = 0 falsy
+
+//direct booleans
+// const something = true // truthy
+// const something = false // falsy
+
+// special values
+// const something = undefined // falsy
+// const something = null //falsy
+// const something = NaN //falsy
+const something = '0'
+if(something) {
+    console.log(`It is working`)
+}
+
+
+
