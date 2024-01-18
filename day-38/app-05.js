53567484d1d00e7c1ff8693dd124598038f71859
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-18 19:06:02 +0530

    javascript: loops

---
+++ day-38/app-05.js
@@ -0,0 +1,14 @@
+
+for(let start = 0; start < 10; start++ ) {
+    console.log('Hi ' + start)
+}
+
+console.log('For Loop Execution Completed')
+
+let cnt = 0
+while(cnt < 10) {
+    console.log('Hello ' + cnt)
+    cnt += 3;
+}
+
+console.log('While Loop Execution Completed')
--- day-38/readme.md
+++ day-38/readme.md
@@ -1,9 +1,6 @@
 We have covered following topics today
 
 - Assignment operators
-    - addition
-    - subtraction
-    - rest is homework
-    - etc
 - Comparision operators
 - bitwise operators
+- loops
