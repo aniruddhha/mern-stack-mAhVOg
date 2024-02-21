7c3a660458bda6ca0d9231d4858e94a04ca03688
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-02-21 18:15:28 +0530

    javascript : call, bind and apply

---
+++ day-52/app-02.js
@@ -0,0 +1,12 @@
+function sayHi(msg, emoji) {
+    console.log(`Hey ${this.nm}, ${msg} ${emoji}` )
+}
+
+
+const obj = { 
+    nm : 'abc'
+}
+
+
+sayHi.call(obj, 'hello', '🚀')
+sayHi.apply(obj, ['hello', '🚀'])
