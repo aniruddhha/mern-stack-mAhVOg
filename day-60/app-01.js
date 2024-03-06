ee5766b640654a73f2d7632df1ff2216d076ec5d
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-03-06 18:25:45 +0530

    javascript: chaining, async, await

--- day-60/app-01.js
+++ day-60/app-01.js
@@ -2,11 +2,10 @@
 // then whatever data i will get, that data i want clean
 
 const prm = new Promise( (res, rej) => {
-
     res( { sts : 'success' })
 } ) // you got the data from facebook 
 
 prm.then( obj => obj) // you cleaned the data
     .then( obj => obj ) // you modified as per requirement
     .then( obj => console.log(obj) ) // you have utlized that data
-    .catch( err => console.error(error))
+    .catch( err => console.error(err))
