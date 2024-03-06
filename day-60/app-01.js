e35746b28feba3589a995eb47a7217015068db00
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-03-06 18:14:38 +0530

    javascript: chaining of promises

---
+++ day-60/app-01.js
@@ -0,0 +1,12 @@
+// i want to get data from facebook, 
+// then whatever data i will get, that data i want clean
+
+const prm = new Promise( (res, rej) => {
+
+    res( { sts : 'success' })
+} ) // you got the data from facebook 
+
+prm.then( obj => obj) // you cleaned the data
+    .then( obj => obj ) // you modified as per requirement
+    .then( obj => console.log(obj) ) // you have utlized that data
+    .catch( err => console.error(error))
