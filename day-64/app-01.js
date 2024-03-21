e7d1835df83112f962e08eef2e4052dc05c039a7
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-03-21 18:27:43 +0530

    dom: query selector

---
+++ day-64/app-01.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // const el = document.querySelector('body :nth-child(3)')
+    // const el = document.querySelector('#p1')
+    const el = document.querySelector('.lng')
+    console.log(el.innerText)
+})
