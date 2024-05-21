28284ef228931a1a552b2a80ca736a23096c97ed
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-21 19:09:25 +0530

    react: initial session

---
+++ day-90/hello-react/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Simple = () => <h1 style={{border : '1px solid red'}}> Hey Hi</h1>
+
+function Normal() {
+    return  <h1 style={{border : '1px solid red'}}> Hey Hi</h1>
+}
+
+const div = document.getElementById('root')
+ReactDOM.createRoot(div)
+        .render( <Normal/> )
+
