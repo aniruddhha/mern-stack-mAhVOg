965abe0f086a9620694d74b196599cba26406fcc
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-28 18:49:00 +0530

    react : component and jsx

---
+++ day-91-92/react-tailwind/src/App.jsx
@@ -0,0 +1,15 @@
+import { Alert } from "./Alert"
+
+export default function App() {
+  return (
+    <div>
+      <h1> Hey Hi </h1>
+      <h1> Hey Hi </h1>
+      <input type="text" className=''/>
+
+      <Alert/>
+    </div>
+  )
+}
+
+
