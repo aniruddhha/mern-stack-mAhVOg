64a35aa1b76a61d30f8c5745d601745d8f33a573
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-07-25 18:07:47 +0530

    react: form handling

---
+++ day-109/form-handling/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+
+function App() {
+
+  const [validation, setValidation] = useState({
+    isUserValid: false,
+    isPasswordValid: false
+  })
+  const [user, setUser] = useState({
+    userName: '',
+    password: ''
+  })
+
+  const handleUsNmChange = (e) => {
+    const { name, value } = e.target
+    console.log(`User Name Changed `)
+
+    setUser({ ...user, userName: value })
+    applyValidation()
+  }
+  
+  const handlePassChange = (e) => {
+    const { name, value } = e.target
+    console.log(`Password Changed`)
+    setUser({ ...user, password: value })
+    applyValidation()
+  }
+
+  const applyValidation = () => {
+    console.log(`UsNm ${user.userName.length}`)
+    console.log(`Pass ${user.password.length}`)
+    if(user.userName.length > 4) {
+      setValidation({ ...validation, isUserValid: true })
+    } else {
+      setValidation({ ...validation, isUserValid: false })
+    }
+
+    if(user.password.length > 4) {
+      setValidation({ ...validation, isPasswordValid: true })
+    } else {
+      setValidation({ ...validation, isPasswordValid: false })
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    applyValidation()
+
+    if(validation.isPasswordValid && validation.isUserValid)
+      console.log(user)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {JSON.stringify(validation)}
+      <div>
+        <label> 
+          Username 
+          <input 
+            name="userName" 
+            type="text" 
+            placeholder='Username' 
+            value={user.userName} 
+            onChange={handleUsNmChange}/>
+        </label>
+        { !validation.isUserValid && <label style={{color: 'red'}}>Username is Mandetory</label>}
+      </div>
+      <div>
+        <label> 
+          Password 
+          <input 
+            name="password" 
+            type="password" 
+            placeholder='Password' 
+            value={user.password} 
+            onChange={handlePassChange}/>
+            { !validation.isPasswordValid && <label style={{color: 'red'}}>Password is Mandetory</label> }
+        </label>
+      </div>
+      <div>
+        <input type="submit" value="Login" />
+      </div>
+    </form>
+  )
+}
+
+export default App
