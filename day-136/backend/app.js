2d4aa141b418bc84f4677d916d84ff2fa499665b
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-06 19:08:05 +0530

    docker : removed error in code

--- day-136/backend/app.js
+++ day-136/backend/app.js
@@ -3,9 +3,9 @@ const app = express()
 app.use(express.json())
 const port = 3000
 
-app.post('/create', (req, res)) => {
+app.post('/create', (req, res) => {
    res.json({ msg: 'Resource created Successfully' })
-}
+})
 
 app.get('/', (req, res) => {
     res.json( { msg: 'Here is the list of resources' })
