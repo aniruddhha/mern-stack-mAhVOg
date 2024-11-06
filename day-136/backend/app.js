7a52dba5dcf6424cc0a33fa746b2a966a3f5b0f0
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-06 19:07:24 +0530

    docker basic setup

---
+++ day-136/backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = 3000
+
+app.post('/create', (req, res)) => {
+   res.json({ msg: 'Resource created Successfully' })
+}
+
+app.get('/', (req, res) => {
+    res.json( { msg: 'Here is the list of resources' })
+})
+
+app.listen(port, () => {
+    console.log(`Express Server is`)
+} )
